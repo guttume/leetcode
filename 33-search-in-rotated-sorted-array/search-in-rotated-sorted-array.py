b21103dcc14adc52339731b80802cb92300dbcea
Amit Kumar Dubey <guttume@outlook.com>
2024-04-17 00:05:27 +0530

    Time: 47 ms (29.77%) | Memory: 17 MB (54.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -9,14 +9,14 @@ class Solution:
                 return mid
 
             if nums[mid] >= nums[l]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                else:
+                if target >= nums[l] and target < nums[mid]:
                     r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else:
+                if target > nums[mid] and target <= nums[r]:
                     l = mid + 1
+                else:
+                    r = mid - 1
         
         return -1
