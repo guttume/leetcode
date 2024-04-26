4ed122a8ca4ba7a51bd953f896b71ff723f802fa
Amit Kumar Dubey <guttume@outlook.com>
2024-04-26 21:08:31 +0530

    Time: 101 ms (33.90%) | Memory: 16.7 MB (32.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,16 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def isPermutation(s, t):
-            if len(s) != len(t):
-                return False
+        if len(s1) > len(s2):
+            return False
 
-            return sorted(s) == sorted(t)
+        s1Counter = [0] * 26
+        s2Counter = [0] * 26
+
+        left, right = 0, len(s1) - 1
         
-        for i in range(len(s2) - len(s1) + 1):
-            if isPermutation(s1, s2[i:i+len(s1)]):
-                return True
+        for i in range(len(s1)):
+            s1Counter[ord(s1[i]) - ord('a')] += 1
+            s2Counter[ord(s2[i]) - ord('a')] += 1
 
+        while right < len(s2):
+            matches = 0
+            for i in range(0, 26):
+                if s1Counter[i] == s2Counter[i]:
+                    matches += 1
+            if matches == 26:
+                return True
+            
+            if right == len(s2) -1:
+                break
+            
+            s2Counter[ord(s2[left]) - ord('a')] -= 1
+            left += 1
+            right += 1
+            s2Counter[ord(s2[right]) - ord('a')] += 1
+            
         return False
+
+        
+
 
 
 
