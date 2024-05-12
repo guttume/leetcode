124b5bf052f5c995a8e9b8d1ad76de39ac6e2d1c
Amit Kumar Dubey <guttume@outlook.com>
2024-05-12 17:30:22 +0530

    Time: 908 ms (85.41%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-with cte as
-(select player_id,
-event_date,
-lead(event_date) over (partition by player_id order by event_date)next_date,
-dense_rank() over (partition by player_id order by event_date) as rnk from activity
-)
-select round(sum(case when datediff(next_date,event_date) = 1 then 1 else 0 end)/count(distinct player_id),2)fraction
-from cte where rnk = 1
+WITH cte AS (
+	SELECT
+		player_id,
+		event_date,
+		lead( event_date ) over ( PARTITION BY player_id ORDER BY event_date ) next_date,
+		dense_rank() over ( PARTITION BY player_id ORDER BY event_date ) AS rnk 
+	FROM
+		Activity 
+	) SELECT
+	round( sum( CASE WHEN datediff( next_date, event_date ) = 1 THEN 1 ELSE 0 END )/ count( DISTINCT player_id ), 2 ) fraction 
+FROM
+	cte 
+WHERE
+	rnk = 1
