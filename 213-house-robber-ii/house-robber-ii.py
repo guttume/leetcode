796001e30d64825d3fa327b5f99ae9e11860b8b0
Amit Kumar Dubey <guttume@outlook.com>
2024-06-16 14:53:52 +0530

    Time: 35 ms (65.32%) | Memory: 16.4 MB (94.02%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,31 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         def helper(nums):
-            if len(nums) == 0:
-                return 0
+            rob1 = 0
+            rob2 = 0
 
-            if len(nums) == 1:
-                return nums[0]
-
-            rob1 = nums[0]
-            rob2 = max(nums[0], nums[1])
-
-            for i in range(2, len(nums)):
-                curr = max(rob2, nums[i] + rob1)
-                rob1 = rob2
-                rob2 = curr
+            for num in nums:
+                rob1, rob2 = rob2, max(rob2, num + rob1)
 
             return max(rob1, rob2)
-        
-        n = len(nums)
 
-        if n == 0:
-            return 0
-
-        if n == 1:
+        if len(nums) == 1:
             return nums[0]
 
-        take = nums[0] + helper(nums[2:-1])
-        notake = 0 + helper(nums[1:])
-
-        return max(take, notake)
+        return max(helper(nums[:-1]), helper(nums[1:]))
