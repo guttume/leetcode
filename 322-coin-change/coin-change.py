4fb9f65d170453e02d8c3507afc1ddfbfee6d7ee
Amit Kumar Dubey <guttume@outlook.com>
2024-06-17 15:29:50 +0530

    Time: 795 ms (55.03%) | Memory: 16.9 MB (82.63%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,28 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # Dictionary to store the results of subproblems
-        memo = {}
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
 
-        def dp(n):
-            # Base cases
-            if n in memo:
-                return memo[n]
-            if n == 0:
-                return 0
-            if n < 0:
-                return -1
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i - c] + 1)
 
-            # Initialize the minimum number of coins to infinity
-            min_coins = float('inf')
-
-            # Try each coin and find the minimum number of coins needed
-            for coin in coins:
-                res = dp(n - coin)
-                if res >= 0 and res < min_coins:
-                    min_coins = res + 1
-
-            # Store the result in the memo dictionary
-            memo[n] = min_coins if min_coins != float('inf') else -1
-            return memo[n]
-
-        return dp(amount)
+        return dp[amount] if dp[amount] != float("inf") else -1
