45fb2e136b289a05732eec3ee2fb7d3615ee876d
Amit Kumar Dubey <guttume@outlook.com>
2024-06-23 12:34:07 +0530

    Time: 349 ms (78.18%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -2,9 +2,8 @@
 select 
     e.employee_id
 from Employees e 
-    left join Employees m on e.manager_id = m.employee_id 
     where e.manager_id is not null
     and e.salary < 30000 
-    and m.employee_id is null 
+    and e.manager_id not in (select employee_id from Employees)
 order by 
     e.employee_id
