5c8117b5f0140314f555b05a50dacd9840072a98
Amit Kumar Dubey <guttume@outlook.com>
2024-06-25 22:49:11 +0530

    Time: 34 ms (72.92%) | Memory: 16.5 MB (81.19%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,6 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+
         cache = {len(s): 1}
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                cache[i] = 0
+            else:
+                cache[i] = cache[i + 1]
+
+            if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                cache[i] += cache[i + 2]
+
+        return cache[0]
+
         def dfs(i):
             if i in cache:
                 return cache[i]
@@ -14,7 +27,7 @@ class Solution:
                 takeTwo = dfs(i+2)
 
             cache[i] = takeOne + takeTwo
-            
+
             return cache[i]
 
         return dfs(0)
