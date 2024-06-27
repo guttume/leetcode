db53a13b455b9720f26507d240037ffc29b9de16
Amit Kumar Dubey <guttume@outlook.com>
2024-06-27 15:49:03 +0530

    Time: 331 ms (96.06%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select today.id from Weather previous join Weather today on DATE_SUB(today.recordDate, INTERVAL 1 DAY) = previous.recordDate where today.temperature > previous.temperature
+with cte as (
+select *, LAG(recordDate) OVER(order by recordDate) as prev_d, LAG(temperature) OVER(order by recordDate) as prev from Weather
+)
+
+select id from cte where temperature > prev and date_sub(recordDate, INTERVAL 1 DAY) = prev_d
