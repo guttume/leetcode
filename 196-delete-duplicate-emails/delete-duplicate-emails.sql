030081d24d1e775c4ae3d5e2a41bdfcfec9131a2
Amit Kumar Dubey <guttume@outlook.com>
2024-07-07 21:59:58 +0530

    Time: 729 ms (89.10%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-WITH cte AS (
-    SELECT *, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) as row_value FROM Person
-)
+-- WITH cte AS (
+--     SELECT *, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) as row_value FROM Person
+-- )
 
-DELETE FROM Person WHERE id in (SELECT id FROM cte WHERE row_value > 1)
+-- DELETE FROM Person WHERE id in (SELECT id FROM cte WHERE row_value > 1)
+
+delete p1 from person p1,person p2 
+where p1.email=p2.email and p1.id>p2.id;
