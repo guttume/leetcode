ac96921b16e9ec4cd44f5ba813ac1908c5076330
Amit Kumar Dubey <guttume@outlook.com>
2024-09-03 09:32:09 +0530

    Time: 36 ms (73.05%) | Memory: 16.6 MB (37.00%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,21 +1,20 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        ints = ""
-
+        digit_sum = 0
         for c in s:
-            ints += str(ord(c) - ord('a') + 1)
-
-        ints = int(ints)
-
-        
-        while k > 0:
-            ans = 0
-            while ints > 0:
-                ints, rem = divmod(ints, 10)
-                ans += rem
+            val = int(str(ord(c) - ord('a') + 1))
+            while val > 0:
+                val, rem = divmod(val, 10)
+                digit_sum += rem
+    
+        while k > 1 and digit_sum >= 10:
+            temp = 0
+            while digit_sum > 0:
+                digit_sum, rem = divmod(digit_sum, 10)
+                temp += rem
             k -= 1
-            ints = ans
+            digit_sum = temp
 
-        return ints
+        return digit_sum
 
         
