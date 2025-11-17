57608ab4b2912219ed79c819255649b035a3cde9
Amit Kumar Dubey <guttume@outlook.com>
2025-11-18 01:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (76.51%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,25 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        size = 26 * 2
-        track = [0] * size
-        done = False
-        ans = 0
+        c = Counter(s)
 
-        for letter in s:
-            pos = ord(letter) - ord('a') if letter in 'abcdefghijklmnopqrstuvwxyz' else 26 + ord(letter) - ord('A')
-            track[pos] += 1
-            
-        for i in range(size):
-            if track[i] % 2 == 0:
-                continue
-            if track[i] % 2 == 1:
-                if done == False:
-                    done = True
-                    continue
-                else:
-                    track[i] -= 1
-                    
-        for i in range(size):
-            ans += track[i]
-            
-        return ans
+        ans = 0
+        odd_used = False
+
+        for i in c.values():
+            if i % 2 == 0:
+                ans += i
+            else:
+                ans += i - 1
+                odd_used = True
+
+        return ans + (1 if odd_used else 0)
