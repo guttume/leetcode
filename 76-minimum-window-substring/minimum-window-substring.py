589e015e3621f2a95374de0cf090179140ccb2ca
Amit Kumar Dubey <guttume@outlook.com>
2025-11-18 22:59:30 +0530

    Time: 543 ms (9.30%) | Memory: 18.3 MB (33.93%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,47 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        ns = len(s)
-        nt = len(t)
-        if ns < nt:
-            return ""
-        
-        tCount = collections.Counter(t)
-        sCount = collections.Counter(s[:nt])
-        matches = 0
-
-        for key, value in tCount.items():
-            if key in sCount:
-                matches += min(sCount[key], value)
-
-        ans = ""
-        min_length = float("inf")
-        l, r = 0, nt
-        
-        while r <= ns:
-            if matches >= nt:
-                current_length = r - l
-                if current_length < min_length:
-                    min_length = current_length
-                    ans = s[l:r]
-                
-                l_key = s[l]    
-                sCount[l_key] -= 1
-                if l_key in tCount and tCount[l_key] > sCount[l_key]:
-                    matches -= 1
-                if sCount[l_key] == 0:
-                    del(sCount[l_key])
-                l += 1
-            else:
-                if r < ns:
-                    r_key = s[r]
-                    sCount[r_key] = 1 + sCount.get(r_key, 0)
-                    if r_key in tCount and sCount[r_key] <= tCount[r_key]:
-                        matches += 1
-                r += 1
-                        
+        def is_t_in_s(s_counter, t_counter):
+            for k, v in t_counter.items():
+                if s_counter.get(k, 0) < v:
+                    return False
                     
+            return True
+
+        if not s or not t:
+            return ""
+            
+        l, r = 0, 0
+        
+        ans = ""
+        min_count = float("inf")
+        
+        t_count = Counter(t)
+        s_count = {}
+        
+        while r < len(s):
+            s_count[s[r]] = 1 + s_count.get(s[r], 0)
+            
+            while is_t_in_s(s_count, t_count):
+                if min_count > r - l + 1:
+                    min_count = r - l + 1
+                    ans = s[l:r+1]
+                    
+                s_count[s[l]] = s_count[s[l]] - 1
+                if s_count[s[l]] == 0:
+                    del(s_count[s[l]])
+                l += 1
+                
+
+            r += 1
+                        
         return ans
-
-
-
-
